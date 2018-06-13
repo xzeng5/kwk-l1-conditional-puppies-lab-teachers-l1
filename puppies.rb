808bab08acc3ebcc468d56acd76d5c2a5fe34e9e
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-13 21:06:39 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -44,3 +44,9 @@ end
   # Write a method called new_animal accumulation that takes in two arguments - number of puppies and number of cats.
   # As long as she gets zero puppies OR zero cats this method should return "Good job!".
   # If she gets 1 or more puppies AND 1 or more cats it should tell her she should really slow back down!
+
+def both_animals (number_puppies, number_cats)
+  if number_puppies == 0 || number_cats == 0
+    "Good job, Rachel!"
+  elsif number_puppies == 1 && number_cats == 1
+end
